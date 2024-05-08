8cf16a857d8f27fa11a3a972b3e242fc2de4bc79
itony93 <itony3561@gmail.com>
2024-05-08 08:02:54 +0530

    computer

---
+++ Animated.html/main.js
@@ -0,0 +1,27 @@
+var typed = new Typed(".text",{
+    strings : ["Web Developer",  "Programmer", "Web Designer"],
+    typeSpeed : 150,
+      backSpeed : 150,
+      looped : true
+})
+
+
+
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem=>{
+var dots = elem.getAttribute("data-dots");
+var marked = elem.getAttribute("data-percent");
+var percent = Math.floor(dots*marked/100);
+var points = "";
+var rotate = 360 / dots;
+
+for(let i = 0 ; i < dots ; i++){
+    points +=  `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`
+}
+    elem.innerHTML = points;
+
+    const pointsMarked = elem.querySelectorAll('.points');
+    for(let i = 0; i<percent ; i++){
+        pointsMarked[i].classList.add('marked')
+    }
+})
